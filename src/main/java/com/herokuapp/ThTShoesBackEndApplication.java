7d1a1de5d7615a364544eea82be8826e2490c2f4
dangthai121200 <dangthai121200@gmail.com>
2022-05-29 11:19:09 +0700

    first commit

---
+++ src/main/java/com/herokuapp/ThTShoesBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.herokuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThTShoesBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThTShoesBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herokuapp/ThTShoesBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herokuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThTShoesBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
