9de41492788bb14957862f5b32ab25d7c4608b03
dangthai121200 <dangthai121200@gmail.com>
2022-08-07 20:10:33 +0700

    fix regex for password

--- src/main/java/com/herokuapp/util/Regex.java
+++ src/main/java/com/herokuapp/util/Regex.java
@@ -1,5 +1,5 @@
 package com.herokuapp.util;
 
 public class Regex {
-	public static final String PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&.])[A-Za-z\\d@$!%*?&.]{8,}$";
+	public static final String PASSWORD = "^.(?=.{8,})((?=.[!@#$%^&()-_=+{};:,<.>]){1})(?=.\\d)((?=.[a-z]){1})((?=.[A-Z]){1}).*$";
 }
